8119926d4584c3ac31c2b323821a64417b909aae
royceraspa <154722178+royceraspa@users.noreply.github.com>
2023-12-30 19:45:27 -0500

    Update server.py

--- server.py
+++ server.py
@@ -51,4 +51,4 @@ def transfer_balance():
         return jsonify({'success': False, 'message': 'Invalid sender or recipient username'}), 400
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    app.run(debug=True, host='10.0.0.69', port=5000)
